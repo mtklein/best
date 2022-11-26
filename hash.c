c3f066700356ae1569edb08d9eafbcb8529be0db
Mike Klein <mtklein@gmail.com>
2022-11-26 12:49:12 -0500

    store mask

--- hash.c
+++ hash.c
@@ -4,14 +4,14 @@
 #include <stdlib.h>
 
 struct hash {
-    int len,cap;
+    int len,mask;
     struct {
         int hash,val;
     } table[];
 };
 
 static void just_insert(struct hash *h, int hash, int val) {
-    int const mask = (h ? h->cap : 0) - 1;
+    int const mask = h ? h->mask : -1;
     assert(h);
     assert(hash);
     for (int n = 0, i = hash&mask; n++ <= mask; i = (i+1)&mask) {
@@ -27,12 +27,12 @@ static void just_insert(struct hash *h, int hash, int val) {
 
 struct hash* hash_insert(struct hash *h, int user, int val) {
     int const hash = user ? user : 1,
-               len = h ? h->len : 0,
-               cap = h ? h->cap : 0;
+               len = h ? h->len    : 0,
+               cap = h ? h->mask+1 : 0;
     if (len >= 3*cap/4) {
         int const growth = cap ? 2*cap : 1;
         struct hash *grown = calloc(1, sizeof *grown + (size_t)growth * sizeof *grown->table);
-        grown->cap = growth;
+        grown->mask = growth-1;
 
         for (int i = 0; i < cap; i++) {
             if (h->table[i].hash) {
@@ -49,7 +49,7 @@ struct hash* hash_insert(struct hash *h, int user, int val) {
 
 bool hash_lookup(struct hash const *h, int user, bool(*match)(int val, void *ctx), void *ctx) {
     int const hash = user ? user : 1,
-              mask = (h ? h->cap : 0) - 1;
+              mask = h ? h->mask : -1;
     for (int n = 0, i = hash&mask; n++ <= mask; i = (i+1)&mask) {
         if (h->table[i].hash == 0) { break; }
         if (h->table[i].hash == hash && match(h->table[i].val, ctx)) { return true; }
