6fb03173d930cc718a3b67dd76c03c21919631ed
Mike Klein <mtklein@gmail.com>
2022-11-27 13:47:47 -0500

    simple

--- hash.c
+++ hash.c
@@ -1,68 +1,49 @@
 #include "hash.h"
-#include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-struct nonzero {
-    unsigned hash;
-};
-static struct nonzero nonzero(unsigned hash) {
-    return (struct nonzero){hash ? hash : 1};
-}
-
 struct hash {
-    unsigned len,mask;
-    struct {
-        unsigned hash_or_0;
-        int      val;
-    } table[];
+    unsigned len,cap;
+    struct { unsigned hash; int val; } table[];
 };
 
-static bool empty_slots_at_least(struct hash const *h, unsigned k) {
-    unsigned const cap = h->mask+1;
-    return h->len+k <= cap;
-}
-
-static void insert(struct hash *h, struct nonzero nz, int val) {
-    assert(empty_slots_at_least(h,2));
-    for (unsigned i = nz.hash & h->mask; ; i = (i+1) & h->mask) {
-        if (h->table[i].hash_or_0 == 0) {
-            h->table[i].hash_or_0 = nz.hash;
-            h->table[i].val       = val;
+static void insert(struct hash *h, unsigned hash, int val) {
+    for (unsigned i = hash % h->cap; ; i = (i+1) % h->cap) {
+        if (h->table[i].hash == 0) {
+            h->table[i].hash = hash;
+            h->table[i].val  = val;
             h->len++;
             break;
         }
     }
-    assert(empty_slots_at_least(h,1));
 }
 
 struct hash* hash_insert(struct hash *h, unsigned hash, int val) {
-    unsigned const len = h ? h->len    : 0,
-                   cap = h ? h->mask+1 : 0;
+    hash = hash ? hash : 1;
+    unsigned const len = h ? h->len : 0,
+                   cap = h ? h->cap : 0;
     if (len >= 7*cap/8) {
         unsigned const growth = cap ? 2*cap : 8;
         struct hash   *grown  = calloc(1, sizeof *grown + growth * sizeof *grown->table);
-        grown->mask = growth-1;
+        grown->cap = growth;
         for (unsigned i = 0; i < cap; i++) {
-            if (h->table[i].hash_or_0 != 0) {
-                insert(grown, nonzero(h->table[i].hash_or_0), h->table[i].val);
+            if (h->table[i].hash) {
+                insert(grown, h->table[i].hash, h->table[i].val);
             }
         }
-        assert(grown->len == len);
         free(h);
         h = grown;
     }
-    insert(h,nonzero(hash),val);
+    insert(h,hash,val);
     return h;
 }
 
 bool hash_lookup(struct hash const *h, unsigned hash, bool(*match)(int, void*), void *ctx) {
+    hash = hash ? hash : 1;
     if (h) {
-        assert(empty_slots_at_least(h,1));
-        struct nonzero const nz = nonzero(hash);
-        for (unsigned i = nz.hash & h->mask; ; i = (i+1) & h->mask) {
-            if (h->table[i].hash_or_0 == 0) { break; }
-            if (h->table[i].hash_or_0 == nz.hash && match(h->table[i].val, ctx)) { return true; }
+        for (unsigned i = hash % h->cap; ; i = (i+1) % h->cap) {
+            if (h->table[i].hash == 0) { break; }
+            if (h->table[i].hash == hash && match(h->table[i].val, ctx)) { return true; }
         }
     }
     return false;
