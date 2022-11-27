d1d682bfd56eced63d53f0d81f13858c4a3fb76d
Mike Klein <mtklein@gmail.com>
2022-11-27 14:41:31 -0500

    tweaks

--- hash.c
+++ hash.c
@@ -6,15 +6,14 @@ struct hash {
     struct slot { unsigned hash; int val; } slot[];
 };
 
-static void insert(struct hash *h, unsigned hash, int val) {
-    for (struct slot *s = h->slot + (hash % h->cap), *end = h->slot + h->cap;;) {
-        if (s->hash == 0) {
-            s->hash = hash;
-            s->val  = val;
+static void insert(struct hash *h, struct slot const s) {
+    for (struct slot *it = h->slot + (s.hash % h->cap), *end = h->slot + h->cap;;) {
+        if (it->hash == 0) {
+            *it = s;
             h->len++;
             break;
         }
-        if (++s == end) { s = h->slot; }
+        if (++it == end) { it = h->slot; }
     }
 }
 
@@ -26,27 +25,27 @@ struct hash* hash_insert(struct hash *h, unsigned hash, int val) {
         struct hash   *grown  = calloc(1, sizeof *grown + growth * sizeof *grown->slot);
         grown->cap = growth;
         if (h) {
-            for (struct slot *s = h->slot; s != h->slot + h->cap; s++) {
-                if (s->hash) {
-                    insert(grown, s->hash, s->val);
+            for (struct slot *it = h->slot, *end = h->slot + h->cap; it != end; it++) {
+                if (it->hash) {
+                    insert(grown, *it);
                 }
             }
             free(h);
         }
         h = grown;
     }
-    insert(h, hash ? hash : 1, val);
+    insert(h, (struct slot){hash ? hash : 1, val});
     return h;
 }
 
 _Bool hash_lookup(struct hash const *h, unsigned hash, _Bool(*match)(int, void*), void *ctx) {
     hash = hash ? hash : 1;
     if (h) {
-        for (struct slot const *s = h->slot + (hash % h->cap), *end = h->slot + h->cap;;) {
-            if (s->hash == 0) { break; }
-            if (s->hash == hash && match(s->val, ctx)) { return 1; }
-            if (++s == end) { s = h->slot; }
+        for (struct slot const *it = h->slot + (hash % h->cap), *end = h->slot + h->cap;;) {
+            if (it->hash == 0) { break; }
+            if (it->hash == hash && match(it->val, ctx)) { return (_Bool)1; }
+            if (++it == end) { it = h->slot; }
         }
     }
-    return 0;
+    return (_Bool)0;
 }
