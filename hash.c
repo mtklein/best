7040ed5634eec2b68b0f6fa6fcda1823226e9023
Mike Klein <mtklein@gmail.com>
2022-11-28 12:58:15 -0500

    mostly use mask

--- hash.c
+++ hash.c
@@ -3,7 +3,7 @@
 
 struct hash { unsigned hash; int val; };
 
-static unsigned slots_for(int vals) {
+static unsigned slots(int vals) {
     if (vals == 0) {
         return 0;
     }
@@ -12,23 +12,23 @@ static unsigned slots_for(int vals) {
     return 1u<<(32 - __builtin_clz((unsigned)slots-1));  // Round up to a power of 2.
 }
 
-static void insert(struct hash *h, unsigned slots, unsigned hash, int val) {
-    struct hash *it = h + (hash & (slots-1));
-    for (struct hash const *end = h + slots; it->hash;) {
-        if (++it == end) { it = h; }
+static void insert(struct hash *h, unsigned mask, unsigned hash, int val) {
+    struct hash *it = h + (hash & mask);
+    while (it->hash) {                   // Looking for an empty slot...
+        if (it++ == h+mask) { it = h; }  // Wrap around to front if we would walk off the end.
     }
     *it = (struct hash){hash, val};
 }
 
 struct hash* hash_insert(struct hash *h, int vals, unsigned hash, int val) {
-    unsigned have = slots_for(vals),
-             need = slots_for(vals+1);
+    unsigned have = slots(vals),
+             need = slots(vals+1);
     if (have < need) {
         struct hash *g = calloc(need, sizeof *g);
         if (h) {
-            for (struct hash *it = h, *end = h + have; it != end; it++) {
+            for (struct hash *it = h; it != h+have; it++) {
                 if (it->hash) {
-                    insert(g,need, it->hash, it->val);
+                    insert(g,need-1, it->hash, it->val);
                 }
             }
             free(h);
@@ -36,7 +36,7 @@ struct hash* hash_insert(struct hash *h, int vals, unsigned hash, int val) {
         h = g;
         have = need;
     }
-    insert(h,have, hash ? hash : 1, val);
+    insert(h,have-1, hash ? hash : 1, val);
     return h;
 }
 
@@ -44,10 +44,10 @@ _Bool hash_lookup(struct hash const *h, int vals, unsigned hash,
                   _Bool(*match)(int, void*), void *ctx) {
     hash = hash ? hash : 1;
     if (h) {
-        unsigned const slots = slots_for(vals);
-        for (struct hash const *it = h + (hash & (slots-1)), *end = h + slots; it->hash;) {
+        unsigned const mask = slots(vals)-1;
+        for (struct hash const *it = h + (hash & mask); it->hash;) {
             if (it->hash == hash && match(it->val, ctx)) { return 1; }
-            if (++it == end) { it = h; }
+            if (it++ == h+mask) { it = h; }
         }
     }
     return 0;
