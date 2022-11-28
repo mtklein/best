0183273dc947bb7300b1e5cf592f2450b2bcb55c
Mike Klein <mtklein@gmail.com>
2022-11-28 10:15:00 -0500

    pow2 allocations

--- hash.c
+++ hash.c
@@ -1,4 +1,5 @@
 #include "hash.h"
+#include <assert.h>
 #include <stdlib.h>
 
 struct hash {
@@ -20,10 +21,11 @@ static void insert(struct hash *h, struct slot const s) {
 struct hash* hash_insert(struct hash *h, unsigned hash, int val) {
     unsigned const len = h ? h->len : 0,
                    cap = h ? h->cap : 0;
-    if (len >= 7*cap/8) {
-        unsigned const growth = cap ? 2*cap : 8;
-        struct hash   *grown  = calloc(1, sizeof *grown + growth * sizeof *grown->slot);
-        grown->cap = growth;
+    if (len >= 3*cap/4) {
+        _Static_assert(sizeof *h == sizeof *h->slot, "The first ''slot'' holds len and cap.");
+        unsigned const growth = cap ? 2*(cap+1) : 8;
+        struct hash   *grown  = calloc(growth, sizeof *grown->slot);
+        grown->cap = growth - 1;
         if (h) {
             for (struct slot *it = h->slot, *end = h->slot + h->cap; it != end; it++) {
                 if (it->hash) {
@@ -41,6 +43,7 @@ struct hash* hash_insert(struct hash *h, unsigned hash, int val) {
 _Bool hash_lookup(struct hash const *h, unsigned hash, _Bool(*match)(int, void*), void *ctx) {
     hash = hash ? hash : 1;
     if (h) {
+        assert(h->len < h->cap && "This loop needs an empty slot (it->hash == 0) to terminate.");
         for (struct slot const *it = h->slot + (hash % h->cap), *end = h->slot + h->cap;;) {
             if (it->hash == 0) { break; }
             if (it->hash == hash && match(it->val, ctx)) { return (_Bool)1; }
--- hash_test.c
+++ hash_test.c
@@ -5,27 +5,27 @@
 static _Bool match_pointee(int val, void *ctx) {
     return val == *(int const*)ctx;
 }
-static void test(unsigned (*hash_fn)(int)) {
+static void test(int const n, unsigned (*hash_fn)(int)) {
     struct hash *h = NULL;
-    for (int i = 0; i < 100; i += 2) {
+    for (int i = 0; i < n; i += 2) {
         expect(!hash_lookup(h, hash_fn(i), match_pointee, &i));
         h = hash_insert(h, hash_fn(i), i);
         expect( hash_lookup(h, hash_fn(i), match_pointee, &i));
     }
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < n; i++) {
         expect( hash_lookup(h, hash_fn(i), match_pointee, &i));
         i++;
         expect(!hash_lookup(h, hash_fn(i), match_pointee, &i));
     }
     free(h);
 }
-static void bench(int loops, unsigned (*hash_fn)(int)) {
+static void bench(int const n, unsigned (*hash_fn)(int), int loops) {
     struct hash *h = NULL;
-    for (int i = 0; i < 100; i += 2) {
+    for (int i = 0; i < n; i += 2) {
         h = hash_insert(h, hash_fn(i), i);
     }
     while (loops --> 0) {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < n; i++) {
             (void)hash_lookup(h, hash_fn(i), match_pointee, &i);
         }
     }
@@ -38,15 +38,15 @@ static unsigned murmur3_scramble(int x) {
     bits = (bits << 15) | (bits >> 17);
     return bits * 0x1b873593;
 }
-static unsigned identity(int x) { return (unsigned)x; }
 static unsigned     zero(int x) { (void)x; return 0; }
+static unsigned identity(int x) { return (unsigned)x; }
 
 int main(int argc, char* argv[]) {
-    test(murmur3_scramble);
-    test(identity);
-    test(zero);
+    test(    100, zero);
+    test(  10000, identity);
+    test(1000000, murmur3_scramble);
     if (argc > 1) {
-        bench(atoi(argv[1]), murmur3_scramble);
+        bench(100, murmur3_scramble, atoi(argv[1]));
     }
     return 0;
 }
