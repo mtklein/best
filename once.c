4cc58967add785f3e2b96f186da2fc02b6641f6a
Mike Klein <mtklein@gmail.com>
2022-11-28 21:06:01 -0500

    avoid a few unncessary spins
    Avoid unncessary spins...
       A) when the first load acquires state=2;
       B) when we release *flag=2 after calling fn(ctx);
       C) when the compare-exchange updates state to 2.
    
    These are all a bit interrelated.  The main improvement is from A),
    avoiding a second atomic load when fn(ctx) has already been called.
    The others are gravy: B) happens only once, and C) only when racing.
    
    Note the stronger memory order for the compare-exchange operation:
    without that acquire, scenario C) could see state=2 without having
    ever acquired the effects of fn(ctx).

--- once.c
+++ once.c
@@ -4,10 +4,10 @@
 void once(char _Atomic *flag, void (*fn)(void *ctx), void *ctx) {
     char state = atomic_load_explicit(flag, memory_order_acquire);
     if (state == 0 && atomic_compare_exchange_strong_explicit(flag, &state, 1,
-                                                              memory_order_relaxed,
-                                                              memory_order_relaxed)) {
+                                                              memory_order_acquire,
+                                                              memory_order_acquire)) {
         fn(ctx);
-        atomic_store_explicit(flag, 2, memory_order_release);
+        atomic_store_explicit(flag, state=2, memory_order_release);
     }
-    while (atomic_load_explicit(flag, memory_order_acquire) != 2) /*spin*/;
+    while (state != 2) { state = atomic_load_explicit(flag, memory_order_acquire); }
 }
