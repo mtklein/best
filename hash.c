e35041a7784633c51397224240e2ab5754b0b236
Mike Klein <mtklein@gmail.com>
2022-11-29 05:52:26 -0500

    refactor to avoid need to assert()

--- hash.c
+++ hash.c
@@ -1,26 +1,22 @@
 #include "hash.h"
-#include <assert.h>
 #include <stdlib.h>
-
-static unsigned slots(int vals) {
-    assert(vals);
-    int const empty = vals / 3,                          // Freely tune memory vs performance.
-              slots = vals + (empty ? empty : 1);        // Must maintain at least 1 empty slot.
-    return 1u<<(32 - __builtin_clz((unsigned)slots-1));  // Round up to a power of 2.
-}
 
 static void just_insert(struct hash *h, unsigned hash, int val) {
     unsigned i;
     for (i = hash & h->mask; h->hash[i]; i = (i+1) & h->mask);
     h->hash[i] = hash;
     h->val [i] = val;
-    h->vals++;
 }
 
 void hash_insert(struct hash *h, unsigned user, int val) {
-    unsigned const need = slots(h->vals+1);
+    int const vals = h->vals + 1,
+             empty = vals / 3,                               // Freely tune memory vs performance.
+             slots = vals + (empty ? empty : 1);             // Must keep at least one empty slot.
+    unsigned const need = 1u<<(32 - __builtin_clz((unsigned)slots-1));  // Round up to power of 2.
+
     if (h->mask+1 < need) {
         struct hash grown = {
+            .vals = h->vals,
             .mask = need-1,
             .hash = calloc(need, sizeof *grown.hash),
             .val  = calloc(need, sizeof *grown.val ),
@@ -30,12 +26,12 @@ void hash_insert(struct hash *h, unsigned user, int val) {
                 just_insert(&grown, h->hash[i], h->val[i]);
             }
         }
-        assert(grown.vals == h->vals);
         free(h->hash);
         free(h->val );
         *h = grown;
     }
     just_insert(h, user ? user : 1, val);
+    h->vals++;
 }
 
 _Bool hash_lookup(struct hash const *h, unsigned user, _Bool(*match)(int, void*), void *ctx) {
