eb5c6912b91a5b3d3ff31ae9dae289fdc0c6ea29
Mike Klein <mtklein@gmail.com>
2022-11-29 06:55:14 -0500

    more formatting

--- hash.c
+++ hash.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 static void just_insert(struct hash *h, unsigned hash, int val) {
-    unsigned *hptr = h->data;
-    int      *vptr = (int*)(hptr + h->slots);
+    unsigned   *hptr = h->data;
+    int        *vptr = (int*)(hptr + h->slots);
     unsigned i, mask = (unsigned)h->slots - 1;
+
     for (i = hash & mask; hptr[i]; i = (i+1) & mask);
     hptr[i] = hash;
     vptr[i] = val;
@@ -15,12 +16,9 @@ void hash_insert(struct hash *h, unsigned user, int val) {
          min_empty = vals / 3,                                 // Freely tune memory vs performance.
              slots = vals + (min_empty ? min_empty : 1),       // Must keep at least one empty slot.
               need = 1<<(32-__builtin_clz((unsigned)slots-1)); // Round up to a power of 2.
+
     if (h->slots < need) {
-        struct hash grown = {
-            .vals  = h->vals,
-            .slots = need,
-            .data  = calloc((unsigned)need, sizeof(unsigned) + sizeof(int)),
-        };
+        struct hash grown = {h->vals, need, calloc((unsigned)need, sizeof(unsigned)+sizeof(int))};
         if (h->vals) {
             unsigned const *hptr = h->data;
             int      const *vptr = (int const*)(hptr + h->slots);
@@ -39,10 +37,11 @@ void hash_insert(struct hash *h, unsigned user, int val) {
 
 _Bool hash_lookup(struct hash h, unsigned user, _Bool(*match)(int, void*), void *ctx) {
     if (h.vals) {
-        unsigned const hash = user ? user : 1,
-                       mask = (unsigned)h.slots - 1;
+        unsigned const  hash = user ? user : 1,
+                        mask = (unsigned)h.slots - 1;
         unsigned const *hptr = h.data;
         int      const *vptr = (int const*)(hptr + h.slots);
+
         for (unsigned i = hash & mask; hptr[i]; i = (i+1) & mask) {
             if (hptr[i] == hash && match(vptr[i], ctx)) { return 1; }
         }
