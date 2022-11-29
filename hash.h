2e74927f0faa249474d7bcde4fa6fc542f30e416
Mike Klein <mtklein@gmail.com>
2022-11-29 05:44:35 -0500

    minor api tweaks

--- hash.h
+++ hash.h
@@ -1,11 +1,11 @@
 #pragma once
 
 struct hash {
-    int      vals;
-    unsigned mask;
+    int       vals;
+    unsigned  mask;
     unsigned *hash;
     int      *val;
 };
 
 void  hash_insert(struct hash      *, unsigned hash, int val);
-_Bool hash_lookup(struct hash const*, unsigned hash, _Bool(*)(int val, void *ctx), void *ctx);
+_Bool hash_lookup(struct hash const*, unsigned hash, _Bool(*match)(int val, void *ctx), void *ctx);
